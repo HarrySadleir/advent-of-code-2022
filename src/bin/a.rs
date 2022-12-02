fce0b3dd30f17c19edec10b44319d6cdcc79ceab
HarrySadleir <hereisharrys@gmail.com>
2022-12-01 17:13:58 -0800

    Day 1

---
+++ src/bin/a.rs
@@ -0,0 +1,16 @@
+
+fn main() {
+    let contents = std::fs::read_to_string("./src/bin/a_input.txt").unwrap();
+
+    let mut sums: Vec<u32> = contents
+        .split("\n\n")
+        .map(|block| 
+            block.split("\n").flat_map(|value| value.parse::<u32>()).sum()
+        ).collect();
+
+    sums.sort_by(|a, b| b.cmp(a));
+
+    println!("Part 1: {:?}", sums[0]);
+    let val: u32 = sums.iter().take(3).sum();
+    println!("Part 2: {:?}", val);
+}
